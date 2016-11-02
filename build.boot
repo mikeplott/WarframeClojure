ec3c04981be356d8e30d0b354be33852fe14f484
michaelplott <mike.plott80@gmail.com>
2016-11-02 19:23:17 -0400

    interfaced with h2 using clojure, having some trouble executing a prepared statement but should be resolved soon.

--- build.boot
+++ build.boot
@@ -4,17 +4,19 @@
                   [nightlight "1.1.0" :scope "test"]
                   [ring "1.4.0"]
                   [hiccup "1.0.5"]
-                  [compojure "1.5.1"]])
+                  [compojure "1.5.1"]
+                  [com.h2database/h2 "1.3.170"]
+                  [funcool/clojure.jdbc "0.9.0"]])
 
 (require '[nightlight.boot :refer [nightlight]])
 
 (deftask build []
   (comp
-    (aot :namespace '#{clojure-purchases.core})
-    (pom :project 'clojure-purchases
+    (aot :namespace '#{clojure-warframe.core})
+    (pom :project 'clojure-warframe
          :version "1.0.0")
     (uber)
-    (jar :main 'clojure-purchases.core)
+    (jar :main 'clojure-warframe.core)
     (target)))
 
 (deftask run []
--- src/clojure_warframe/core.clj
+++ src/clojure_warframe/core.clj
@@ -2,7 +2,9 @@
   (:require [clojure.string :as str]
             [compojure.core :as c]
             [ring.adapter.jetty :as j]
-            [hiccup.core :as h])
+            [hiccup.core :as h]
+            ;[clojure.java.jdbc :as sql]
+            [jdbc.core :as jdbc])
   (:gen-class))
 
 (def file-name "items.txt")
@@ -15,10 +17,36 @@
         header (first items)
         items (rest items)
         items (map (fn [item]
-                     (zipmap header line))
+                     (zipmap header item))
                 items)]
     items))
+
+(def db
+  {:classname "org.h2.Driver"
+   :subprotocol "h2:file"
+   :subname "db/warframe"})
+
+(with-open [conn (jdbc/connection db)]
+  (jdbc/execute conn "CREATE TABLE IF NOT EXISTS item_list (id IDENTITY, item_name VARCHAR, category VARCHAR, void_relic VARCHAR);")
+  (jdbc/execute conn "CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, password VARCHAR);")
+  (jdbc/execute conn "CREATE TABLE IF NOT EXISTS user_list (id IDENTITY, item_name VARCHAR, category VARCHAR, void_relic VARCHAR, quantity INT, user_id INT);"))
+  
+
+;(sql/with-connection db 
+ ; (sql/create-table :items
+  ;  [:id "bigint primary key auto_increment"]
+   ; [:item_name "varchar"]
+    ;[:category "varchar"]
+    ;[:void_relic "varchar"]})
+(with-open [conn (jdbc/connection db)]
+  (let [items (read-items)
+        items {:item_name (get items "item_name") 
+               :category (get items "category")
+               :void_relic (get items "void_relic")}]
+    (jdbc/execute conn "INSERT INTO item_list VALUES(NULL, ?, ?, ?)" :item_name, :category, :void_relic)))
+    
+
                 
 
 (defn -main [& args]
-  (println "Hello world!"))
+ (j/run-jetty app {:port 3000}))
