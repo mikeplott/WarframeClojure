1b4bd743ca12b8e793cb913338f6bc3c7b83b849
michaelplott <mike.plott80@gmail.com>
2016-11-03 17:15:44 -0400

    still working on full db functionality. I believe the create tables function works but working on a test select all function to return data to assert true.

--- src/clojure_warframe/core.clj
+++ src/clojure_warframe/core.clj
@@ -11,25 +11,52 @@
 
 (defn read-items []
   (let [items (str/split-lines (slurp file-name))
-        items (map (fn [item] 
-                     (str/split item #"|"))
+        items (map (fn [line] 
+                     (str/split line #"\|"))
                 items)
         header (first items)
         items (rest items)
-        items (map (fn [item]
-                     (zipmap header item))
+        items (map (fn [line]
+                     (zipmap header line))
                 items)]
     items))
+
+(def num 1)
 
 (def db
   {:classname "org.h2.Driver"
    :subprotocol "h2:file"
    :subname "db/warframe"})
 
-(with-open [conn (jdbc/connection db)]
-  (jdbc/execute conn "CREATE TABLE IF NOT EXISTS item_list (id IDENTITY, item_name VARCHAR, category VARCHAR, void_relic VARCHAR);")
-  (jdbc/execute conn "CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, password VARCHAR);")
-  (jdbc/execute conn "CREATE TABLE IF NOT EXISTS user_list (id IDENTITY, item_name VARCHAR, category VARCHAR, void_relic VARCHAR, quantity INT, user_id INT);"))
+(defn create-tables []
+  (with-open [conn (jdbc/connection db)]
+    (jdbc/execute conn "CREATE TABLE IF NOT EXISTS item_list (id IDENTITY, item_name VARCHAR, category VARCHAR, void_relic VARCHAR);"
+      (let [items (read-items)]
+        (map (fn [item]
+               (jdbc/execute conn) 
+               ["INSERT INTO item_list VALUES(NULL, ?, ?, ?);"] 
+               (get item "item_name"),
+               (get item "category"),
+               (get item "void_relic"))
+          items)))
+    (jdbc/execute conn "CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, password VARCHAR);")
+    (jdbc/execute conn "CREATE TABLE IF NOT EXISTS user_list (id IDENTITY, item_name VARCHAR, category VARCHAR, void_relic VARCHAR, quantity INT, user_id INT);")))
+    
+(defn item-import []
+  (let [items (read-items)]
+    (with-open [conn (jdbc/connection db)]
+      jdbc/execute conn ["INSERT INTO item_list VALUES(NULL, ?, ?, ?)"
+                         (get items "item_name"), 
+                         (get items "category"), 
+                         (get items "void_relic")])
+    items))
+
+(defn test-import []
+  (with-open [conn (jdbc/connection db)]
+    (jdbc/execute conn ["SELECT * FROM item_list"])))
+     
+     
+      
   
 
 ;(sql/with-connection db 
@@ -38,15 +65,15 @@
    ; [:item_name "varchar"]
     ;[:category "varchar"]
     ;[:void_relic "varchar"]})
-(with-open [conn (jdbc/connection db)]
-  (let [items (read-items)
-        items {:item_name (get items "item_name") 
-               :category (get items "category")
-               :void_relic (get items "void_relic")}]
-    (jdbc/execute conn "INSERT INTO item_list VALUES(NULL, ?, ?, ?)" :item_name, :category, :void_relic)))
+;(with-open [conn (jdbc/connection db)]
+;  (let [items (read-items)
+ ;       items {:item_name (get items "item_name") 
+ ;              :category (get items "category")
+ ;              :void_relic (get items "void_relic")
+ ;   (jdbc/execute conn "INSERT INTO item_list VALUES(NULL, ?, ?, ?)" :item_name, :category, :void_relic)))
     
 
                 
 
-(defn -main [& args]
- (j/run-jetty app {:port 3000}))
+(defn -main [& args])
+ ;(j/run-jetty app {:port 3000}))
