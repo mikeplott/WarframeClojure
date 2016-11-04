20b2234a0d5f52ddc0881ff79ac03e5f758283f7
michaelplott <mike.plott80@gmail.com>
2016-11-03 20:00:33 -0400

    Database functionality working, using both postgresql and h2, all file import functions work.

--- build.boot
+++ build.boot
@@ -6,7 +6,9 @@
                   [hiccup "1.0.5"]
                   [compojure "1.5.1"]
                   [com.h2database/h2 "1.3.170"]
-                  [funcool/clojure.jdbc "0.9.0"]])
+;                 [funcool/clojure.jdbc "0.9.0"]
+                  [org.clojure/java.jdbc "0.6.2-alpha3"]
+                  [org.postgresql/postgresql "9.4.1207"]])
 
 (require '[nightlight.boot :refer [nightlight]])
 
--- src/clojure_warframe/core.clj
+++ src/clojure_warframe/core.clj
@@ -3,8 +3,8 @@
             [compojure.core :as c]
             [ring.adapter.jetty :as j]
             [hiccup.core :as h]
-            ;[clojure.java.jdbc :as sql]
-            [jdbc.core :as jdbc])
+            [clojure.java.jdbc :as sql])
+            ;[jdbc.core :as jdbc])
   (:gen-class))
 
 (def file-name "items.txt")
@@ -20,42 +20,105 @@
                      (zipmap header line))
                 items)]
     items))
-
-(def num 1)
-
-(def db
+                      
+(def db-spec
   {:classname "org.h2.Driver"
    :subprotocol "h2:file"
    :subname "db/warframe"})
+  
+
+(def the-db
+  {:classname "org.postgresql.Driver"
+   :subprotocol "postgresql"
+   :subname "//localhost:5432/warframe"
+   :user "michaelplott"
+   :password "dubose"})
+   
+
+(defn create-ext-tables []
+   (sql/db-do-commands the-db
+      (sql/create-table-ddl 
+        :item_list [[:id "SERIAL"]
+                    [:item_name "TEXT"] 
+                    [:category "TEXT"]
+                    [:void_relic "TEXT"]])))
+
+
+(defn insert-ext []
+  ;(sql/db-do-commands the-db
+    (let [items (read-items)]
+      (map (fn [item]
+             (sql/insert! the-db
+               :item_list 
+               {:item_name (get item "item_name") 
+                :category (get item "category") 
+                :void_relic (get item "void_relic")})) 
+        items)))
+    
 
 (defn create-tables []
-  (with-open [conn (jdbc/connection db)]
-    (jdbc/execute conn "CREATE TABLE IF NOT EXISTS item_list (id IDENTITY, item_name VARCHAR, category VARCHAR, void_relic VARCHAR);"
-      (let [items (read-items)]
+   (sql/db-do-commands db-spec
+      (sql/create-table-ddl 
+        :item_list [[:id "IDENTITY"]
+                    [:item_name "VARCHAR"] 
+                    [:category "VARCHAR"]
+                    [:void_relic "VARCHAR"]]))
+   (let [items (read-items)]
         (map (fn [item]
-               (jdbc/execute conn) 
-               ["INSERT INTO item_list VALUES(NULL, ?, ?, ?);"] 
-               (get item "item_name"),
-               (get item "category"),
-               (get item "void_relic"))
+               (sql/insert! db-spec 
+                 :item_list 
+                 {:item_name (get item "item_name") 
+                  :category (get item "category") 
+                  :void_relic (get item "void_relic")})) 
           items)))
-    (jdbc/execute conn "CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, password VARCHAR);")
-    (jdbc/execute conn "CREATE TABLE IF NOT EXISTS user_list (id IDENTITY, item_name VARCHAR, category VARCHAR, void_relic VARCHAR, quantity INT, user_id INT);")))
-    
-(defn item-import []
-  (let [items (read-items)]
-    (with-open [conn (jdbc/connection db)]
-      jdbc/execute conn ["INSERT INTO item_list VALUES(NULL, ?, ?, ?)"
-                         (get items "item_name"), 
-                         (get items "category"), 
-                         (get items "void_relic")])
-    items))
 
 (defn test-import []
-  (with-open [conn (jdbc/connection db)]
-    (jdbc/execute conn ["SELECT * FROM item_list"])))
+  (let [test-data
+        (sql/query db-spec ["SELECT * FROM item_list"])]
+    (println test-data)
+    test-data))
+
+(defn -main [& args])
+
+
+
+;(defn data
+  ;(sql/with-db-connection
+ ;   (let [items (read-items)])
+  ;  (map (fn [item]
+   ;        (sql/insert! the-db
+    ;         :item_list 
+     ;        {:item_name (get item "item_name") 
+      ;        :category (get item "category") 
+       ;       :void_relic (get item "void_relic")) 
+     ; items))
+
+
+;               ["INSERT INTO item_list VALUES(NULL, ?, ?, ?);"] 
+;               (get item "item_name"),
+;               (get item "category"),
+;               (get item "void_relic"))
+
+
+    ;(jdbc/execute conn "CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, password VARCHAR);")
+    ;(jdbc/execute conn "CREATE TABLE IF NOT EXISTS user_list (id IDENTITY, item_name VARCHAR, category VARCHAR, void_relic VARCHAR, quantity INT, user_id INT);")))
+    
+;(defn item-import []
+ ; (let [items (read-items)]
+  ;  (with-open [conn (sql/connection db)]
+   ;   sql/execute conn ["INSERT INTO item_list VALUES(NULL, ?, ?, ?)"
+    ;                     (get items "item_name"), 
+     ;                    (get items "category"), 
+      ;                   (get items "void_relic"))
+    ;items))
+
+;(defn test-import []
+ ; (with-open [conn (jdbc/connection db)]
+  ;  (jdbc/execute conn ["SELECT * FROM item_list"])))
      
-     
+;(defn test-import []
+ ; (with-open [conn (jdbc/connection db)]
+  ;  jdbc/query db ["SELECT * FROM item_list"]))    
       
   
 
@@ -75,5 +138,5 @@
 
                 
 
-(defn -main [& args])
+
  ;(j/run-jetty app {:port 3000}))
